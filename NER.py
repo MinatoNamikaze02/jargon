d55928ef5609cb9054653803fc34e743b515d44e
arjun <arjunsreedar@yahoo.com>
2023-07-14 14:14:11 +0530

    minor changes

--- NER.py
+++ NER.py
@@ -10,16 +10,9 @@ RELEASE_DB_NAME = "release_db.sqlite"
 conn = sqlite3.connect(RELEASE_DB_NAME)
 
 policy_texts_df = pd.read_sql_query("SELECT * FROM policy_texts", conn)
-
-policy_texts_df.tail()
-
 simply_the_text = policy_texts_df[["id", "policy_text"]]
 simply_the_text.head()
-
 simply_the_text = simply_the_text[:500]
-
-print(len(simply_the_text))
-
 # use labels.json to label policies in simply_the_text dataframe
 import json
 with open('labels.json') as f:
@@ -27,17 +20,12 @@ with open('labels.json') as f:
 # create a new column in simply_the_text dataframe called "label"
 simply_the_text["label"] = ""
 
-simply_the_text.head()
-
 # for each policy in simply_the_text dataframe, find the corresponding label in labels.json
 for i in range(len(simply_the_text)):
     for title, labels_list in labels.items():
         # if any word in labels_list appears in policy_text, then add the title in the labels. there can be multiple labels for each policy
         if any(word in simply_the_text["policy_text"][i] for word in labels_list):
             simply_the_text["label"][i] +=  ", " + title if simply_the_text["label"][i] != "" else title
-
-simply_the_text.head()
-
 
 import json
 import pandas as pd
@@ -69,11 +57,6 @@ for i, row in simply_the_text.iterrows():
     # Add the annotations to the "annotations" column
     simply_the_text.at[i, "annotations"] = {"entities": annotations}
 
-# Display the updated dataframe
-print(simply_the_text)
-
-simply_the_text.head()
-
 import pandas as pd
 import spacy
 import random
@@ -103,8 +86,13 @@ if "ner" not in nlp.pipe_names:
 else:
     ner = nlp.get_pipe("ner")
 
-# Add custom entity labels to the NER pipeline
-labels = ['business', 'law', 'regulations', 'usability', 'education', 'technology', 'multidisciplinary']
+# get all label titles from labels.json
+import json 
+with open('labels.json') as f:
+    labels = json.load(f)
+labels = list(labels.keys())
+print(labels)
+
 for label in labels:
     ner.add_label(label)
 
@@ -113,8 +101,8 @@ other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
 with nlp.disable_pipes(*other_pipes):
     optimizer = nlp.begin_training()
     batch_sizes = compounding(4.0, 32.0, 1.001)
-    epochs = 10
-
+    epochs = 8
+    all_losses = []
     for epoch in range(epochs):
         random.shuffle(train_data_ner)
         losses = {}
@@ -122,7 +110,13 @@ with nlp.disable_pipes(*other_pipes):
         print(f"Epoch {epoch+1}/{epochs}")
         for batch in batches:
             nlp.update(batch, drop=0.3, losses=losses)
+        all_losses.append(losses)
         print(f"Loss: {losses['ner']:.4f}")
+
+        # if loss increases, stop training
+        if epoch > 0 and losses["ner"] > all_losses[epoch-1]["ner"]:
+            break
+
 
 # Evaluate the model on the testing set
 print("Evaluating the model on the testing set...")
@@ -141,17 +135,32 @@ for example in test_data_ner:
     entities = [(ent.text, ent.label_) for ent in doc.ents]
     predictions.append(entities)
 
-# Evaluate the model's performance using suitable metrics
-print("Model evaluation completed.")
+# Evaluate the predictions
+print("Evaluating the predictions...")
+results = pd.DataFrame(columns=["text", "label", "prediction"])
+for (_, row), prediction in zip(test_data.iterrows(), predictions):
+    text = row["policy_text"]
+    label = row["label"]
+    prediction = ", ".join([f"{ent[0]} ({ent[1]})" for ent in prediction])
+    results = results.add(pd.Series([text, label, prediction], index=results.columns), ignore_index=True)
 
-# Save the trained model for future use
-print("Saving the trained model...")
-nlp.to_disk("trained_ner_model")
-print("Model saved successfully.")
+# Display the evaluation results
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report
 
-# use the model
-import spacy
+# Plot the confusion matrix
+plt.figure(figsize=(20, 20))
+sns.heatmap(pd.crosstab(results["label"], results["prediction"], rownames=["Actual"], colnames=["Predicted"]))
+plt.show()
+
+# Print the classification report
+print(classification_report(results["label"], results["prediction"]))
+
+# Save the trained model
+nlp.to_disk("model")
+
 nlp = spacy.load("trained_ner_model")
 
 # Define a function to predict labels for a given text
